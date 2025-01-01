c94828efbeecff3a319ed79a422e21eb17e5b539
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2025-01-01 23:32:04 +0300

    Add all

---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
+import { Link } from "react-router-dom";
+import './MainNavigation.css';
+
+const MainNavigation = props => {
+
+    const [ drawerIsOpen, setDrawerIsOpen ] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    }
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
+
+    return(
+        <React.Fragment>
+            
+            {/* for smaller screens */}
+
+            {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+
+            {/* {drawerIsOpen ?  */}
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawer}> 
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+            {/* // :  null}; */}
+
+
+            {/* for greater screens */}
+            <MainHeader> 
+                <button className="main-navigation__menu-btn" onClick={openDrawer}>
+                    <span />
+                    <span />
+                    <span />
+                </button>
+
+                <h1 className="main-navigation__title">
+                    <Link to="/">YourPlacesssss</Link>
+                </h1>
+                
+                <nav className="main-navigation__header-nav">
+                    <NavLinks />
+                </nav>
+                
+            </MainHeader>
+        </React.Fragment>
+
+    )
+}
+
+export default MainNavigation;
